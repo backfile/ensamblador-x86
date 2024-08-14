ddbb55cef5b60f356d0a640d2b8bd6f3c4952f0c
Franco Caraffa <francocaraffa2005@gmail.com>
2024-08-14 20:35:43 -0300

    mul

---
+++ 3.mul/mul.asm
@@ -0,0 +1,27 @@
+section .bss
+resultado resb 1
+
+section .text
+	global _start
+_start:
+mov ax, 3
+mov cx, 2
+
+; MUltiplicar 3 x 2
+
+mul cx ; A ax lo multiplica en automatico.
+
+add ax, 48
+mov [resultado], ax
+
+mov eax, 4
+mov ebx, 1
+mov ecx, resultado
+mov edx, 1
+
+int 0x80
+
+mov eax, 1
+mov ebx, 0
+
+int 0x80
